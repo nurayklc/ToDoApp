977fca083d46ad9e1bbfef25871de7fafbf3ba1f
nurayklc <nuriskl72@gmail.com>
2021-10-29 20:14:30 +0300

    fixed home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,7 +4,7 @@ import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home/home.component.html',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
